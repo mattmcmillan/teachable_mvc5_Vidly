1d7f904bd1d8537505c71c477301c8aea4501601
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-04-18 12:24:04 -0700

    -Applied annotations to CustomerName in database -Display Customers from database in Customers view     - Add DiscountRate from MebershipTypes

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -7,16 +7,28 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -24,16 +36,6 @@ namespace Vidly.Controllers
             }
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer() {Id = 1, Name = "Customer 1"},
-                new Customer() {Id = 2, Name = "Customer 2"},
-                new Customer() {Id = 3, Name = "Customer 3"}
-            };
         }
     }
 }
